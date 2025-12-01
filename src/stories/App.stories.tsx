9e1421b94366051b3f0c2a9a8ee117cdb9949632
Callie <96257895+pips90@users.noreply.github.com>
2025-12-01 15:01:21 -0600

    init commit

---
+++ src/stories/App.stories.tsx
@@ -0,0 +1,29 @@
+import type { Meta, StoryObj } from "@storybook/react";
+import App from "../App";
+
+const meta: Meta<typeof App> = {
+  title: "Example/App",
+  component: App,
+  tags: ["autodocs"],
+  decorators: [
+    (Story) => (
+      <div
+        style={{
+          width: 400,
+          margin: "2rem auto",
+          border: "1px solid #eee",
+          padding: 24,
+        }}
+      >
+        <Story />
+      </div>
+    ),
+  ],
+};
+
+export default meta;
+type Story = StoryObj<typeof App>;
+
+export const Default: Story = {
+  render: () => <App />,
+};
